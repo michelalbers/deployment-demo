da92d260d24de91c38be405329f236c1bfbfd5f6
m <michel.albers@gmail.com>
2025-09-24 11:29:44 +0200

    Intial commit

---
+++ src/test/java/com/example/deployment_demo/DeploymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployment_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
